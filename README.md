6174e569e7cb5170cca77452437a3d7f045eb529
s18alg <s18alg@protonmail.com>
2024-08-17 22:56:40 +0200

    init repo

---
+++ README.md
@@ -0,0 +1,10 @@
+# Weird birthday project
+
+## Roadmap
+
+- implement weird ages
+- display more info about weird ages
+- clean up design
+- export to calendar
+- unit test some stuff
+- mobile interface
---
+++ index.js
@@ -0,0 +1,83 @@
+
+const DateTime = luxon.DateTime
+var elementCache = {}
+
+function getElement(id){
+    if (!elementCache[id]) {
+        elementCache[id] = document.getElementById(id)
+    }
+
+    return elementCache[id]
+}
+
+function isPrime(num){
+    const ref = Math.sqrt(num)
+    var i = 3
+
+    if (num % 2 === 0) { return false }
+
+    while (i < ref) {
+        if (num % i === 0) { return false }
+
+        i += 2
+    }
+    return true
+}
+
+// TODO: Days until next birthday
+// TODO: Days since birth
+// TODO: Export function
+const nextFunction = {
+    'next_bd': nextBirthday,
+    'prime_day_bd': nextPrime,
+    'timestamp_day': timestampDay
+}
+
+
+function launchSearch(){
+    var d = getElement('date').value
+    const list = getElement('history')
+
+    // todo: this works now but maybe it could use a link or button to help with clickability
+    var e = document.createElement('li')
+    e.innerHTML = d
+    e.onclick = () => callHistory(e)
+    list.prepend(e)
+
+    callHistory(e)
+}
+
+function callHistory(element){
+    const d = DateTime.fromISO(element.outerText)
+
+    applyResult(d)
+}
+
+function applyResult(date){
+    var now = DateTime.now()
+
+    for (const id in nextFunction) {
+        e = getElement(id)
+
+        e.innerHTML = nextFunction[id](now, date).toLocaleString()
+    }
+}
+
+function nextBirthday(now, base){
+    var age = now.year - base.year + 1
+
+    return base.plus({years: age})
+}
+
+function nextPrime(now, base) {
+    var age = now.year - base.year + 1
+
+    while (!isPrime(age)) {
+        age++
+    }
+    return base.plus({years: age})
+}
+
+function timestampDay(now, base) {
+    return base.plus({second: 1234567890})
+}
